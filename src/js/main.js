d72bc2d151d18da5ea84c425ec48bdb4df886ab2
Marina Benítez Sánchez <marinabsanz@gmail.com>
2021-05-22 12:35:47 +0200

    paintingAPistart

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,40 @@
 
 console.log(">> Ready :)");
 
-const urlShow = "https://api.tvmaze.com/search/shows?q=girls";
+const urlShow = "https://api.tvmaze.com/search/shows?q=";
+const userText = document.querySelector(".js-user-search");
+const button = document.querySelector(".js-button");
+const result = document.querySelector(".js-result");
 
-function searchSHows() {
-  fetch(urlShow)
+function searchShows() {
+  const userResult = userText.value;
+  fetch(urlShow+userResult)
     .then((response) => response.json())
-    .then((data) => {
-      urlShow;
+
+    //he llamado SHOWS a TODO el json que veo en postman
+    //podri ser pepino
+
+    .then((cucumber) => {
+     //añadir mas parametros para trastear
+
+     //>pintar y linkear 
+if (cucumber.length === 0 ) {
+    result.innerHTML += "no hay resultados en tu búsqueda"
+    
+} else { result.innerHTML += "estos son tus resultados" }
+
+
+      for (const userShow of cucumber) {
+        
+       
+       result.innerHTML += "tu serie es "+ userShow.show.name+ ":"  +"<br>"
+       result.innerHTML += userShow.show.image.original
+
+
+       ///TENGO QUE PINTAR LA IMAGEN y meter una de relleno si no hay
+      }
     });
 }
+ //limpiar antes de volver a pintar y hoja de reset
+button.addEventListener("click", searchShows);
+console.log (button);
