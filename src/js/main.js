82f36aa6748245b6bb5451789e629c96afebdc2a
Marina Benítez Sánchez <marinabsanz@gmail.com>
2021-05-22 16:44:34 +0200

    add if else and for

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,42 @@
 "use strict";
 
-console.log(">> Ready :)");
-
 const urlShow = "https://api.tvmaze.com/search/shows?q=";
 const userText = document.querySelector(".js-user-search");
 const button = document.querySelector(".js-button");
 const result = document.querySelector(".js-result");
 
+const urlImageFill = "https://placekitten.com/200/300";
+//VARIABLES VACIAS
+// let cucumber = [];
+// let favoritos = [];
+
 function searchShows() {
   const userResult = userText.value;
-  fetch(urlShow+userResult)
+  fetch(urlShow + userResult)
     .then((response) => response.json())
 
-    //he llamado SHOWS a TODO el json que veo en postman
-    //podri ser pepino
-
+    //he llamado CUCUMBER a TODO el json que veo en postman
     .then((cucumber) => {
-     //añadir mas parametros para trastear
+      //añadir mas parametros para trastear
 
-     //>pintar y linkear 
-if (cucumber.length === 0 ) {
-    result.innerHTML += "no hay resultados en tu búsqueda"
-    
-} else { result.innerHTML += "Estos son tus resultados al buscar " }
+      //>pintar y linkear
+      if (cucumber.length === 0) {
+        result.innerHTML += "no hay resultados en tu búsqueda";
+      } else if  (cucumber.length >= 2 ) {
+        result.innerHTML = "Introduce una letrita más";
+      } else {
+        result.innerHTML += "Estos son tus resultados al buscar ";
+      }
 
       for (const userShow of cucumber) {
-         const yourShow= document.createElement ('li');
-          
-       result.innerHTML += userShow.show.name+":"  +"<br>" 
-      //  result.innerHTML += userShow.show.image.original
+        const lihow = document.createElement("li");
 
-
-       ///TENGO QUE PINTAR LA IMAGEN y meter una de relleno si no hay
+        result.innerHTML += userShow.show.name + ":" + "<br>";
       }
     });
 }
- //limpiar antes de volver a pintar y hoja de reset
+
 button.addEventListener("click", searchShows);
-console.log (button);
+
+///TENGO QUE PINTAR LA IMAGEN y meter una de relleno si no hay
+//limpiar antes de volver a pintar y hoja de reset
