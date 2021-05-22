75bc1b5ee333f9d575a56350e18876b165fa886f
Marina Benítez Sánchez <marinabsanz@gmail.com>
2021-05-22 19:06:13 +0200

    add cimag constants

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,14 @@
 "use strict";
 
+
+
 const urlShow = "https://api.tvmaze.com/search/shows?q=";
 const userText = document.querySelector(".js-user-search");
 const button = document.querySelector(".js-button");
 const result = document.querySelector(".js-result");
 
+////NO ME VAn
+const image = document.querySelector ("js-image")
 const urlImageFill = "https://placekitten.com/200/300";
 //VARIABLES VACIAS
 // let cucumber = [];
@@ -15,23 +19,25 @@ function searchShows() {
   fetch(urlShow + userResult)
     .then((response) => response.json())
 
-    //he llamado CUCUMBER a TODO el json que veo en postman
+    //CUCUMBER --DATA a TODO el json que veo en postman
     .then((cucumber) => {
       //añadir mas parametros para trastear
 
       //>pintar y linkear
       if (cucumber.length === 0) {
+        
         result.innerHTML += "no hay resultados en tu búsqueda";
-      } else if  (cucumber.length >= 2 ) {
+      } else if (cucumber.length <= 2) {
         result.innerHTML = "Introduce una letrita más";
       } else {
         result.innerHTML += "Estos son tus resultados al buscar ";
       }
 
       for (const userShow of cucumber) {
-        const lihow = document.createElement("li");
-
+        const liShow = document.createElement("li");
         result.innerHTML += userShow.show.name + ":" + "<br>";
+       
+        //SALE LA ruta  PERO NO LA IMAGEN-->investigar
       }
     });
 }
