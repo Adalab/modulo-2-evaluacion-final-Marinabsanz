bc4d828723599cfdb72a497af923005e6fe61bd6
Marina Benítez Sánchez <marinabsanz@gmail.com>
2021-05-24 19:42:16 +0200

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,68 +1,90 @@
 "use strict";
 
 const urlShow = "//api.tvmaze.com/search/shows?q=";
+//otra opcion sería fetch(`api.tvmaze.com/search/shows?q=${inputText.value}`)
 
-//otra opcion sería fetch(`http://api.tvmaze.com/search/shows?q=${inputText.value}`)
-//pero abajo he puesto las dos var concatenadas para entender mejor. (más basto)
 const userText = document.querySelector(".js-user-search");
 const button = document.querySelector(".js-button");
 const result = document.querySelector(".js-result");
-//arra de faviritos vacio
-let totalSeries=  [];
+//array de favoritos vacio
+let totalSeries = [];
 let listFav = [];
 
 function searchShows() {
   const userResult = userText.value;
-  //lo de PON la letrita mas iria aqui ---
+  // PON  1 letrita mas ......
+  // if (userResult.length <= 2) {
+  //  result.innerHTML = "escribe una letrita más!"
+  // }
   fetch(urlShow + userResult)
     .then((response) => response.json())
-    //CUCUMBER --DATA a TODO el json que veo en postman
-    .then((cucumber) => {
-      if (cucumber.length === 0) {
+
+    .then((data) => {
+      if (data.length === 0) {
         result.innerHTML = "no hay resultados en tu búsqueda";
       } else {
         result.innerHTML = "Estos son tus resultados :";
       }
 
-      for (const userShow of cucumber) {
+      for (const userShow of data) {
         let htmlDeUnaSerie = "";
-        htmlDeUnaSerie += '<li class="js-result changeColor" id="${userShow.show.id}">';
+        htmlDeUnaSerie +=
+          '<li class="js-list changeColor" id="${userShow.show.id}">';
         htmlDeUnaSerie += userShow.show.name + ":" + "</br>";
         const imagesRslt = userShow.show.image;
         if (imagesRslt === null) {
-          htmlDeUnaSerie = `<img src= "https://placekitten.com/200/300"></img>"`;
+          htmlDeUnaSerie = `<img src= https://www.panatier.es/web/image/product.template/7363/image?unique=d772d4f"></img>"`;
         } else {
           htmlDeUnaSerie += `<img src="${userShow.show.image.medium}"/>`;
         }
         htmlDeUnaSerie += "</li>";
         result.innerHTML += htmlDeUnaSerie;
-        takeList (userShow)   //llamo a la funcion q me llena el array
-        console.log (totalSeries);
-      }   
+        takeList(userShow); //llamo a la funcion q me llena el array
+        console.log(totalSeries);
+      }
     });
 }
 
-function takeList (userShow) {
+function takeList(userShow) {
   if (userShow.show.image === true) {
-    totalSeries.push ({name:` ${userShow.show.name}`, imagen: `${userShow.show.image.medium}` })
-
+    totalSeries.push({
+      name: ` ${userShow.show.name}`,
+      image: `${userShow.show.image.medium}`,
+    });
   } else {
-    totalSeries.push ({name: ` ${userShow.show.name}`, imagen: `https://placekitten.com/200/300` })
-  };
+    totalSeries.push({
+      name: ` ${userShow.show.name}`,
+      image: `https://www.panatier.es/web/image/product.template/7363/image?unique=d772d4f`,
+    });
+  }
 }
-
-
-function funct (evt) {
-  let selectionTotal = evt.currentTarget;
+//coger al hacer click
+function resultTotal(evt) {
+  const goClick = document.querySelectorAll(".js-list");
+  for (const newconst of goClick) {
+    goClick.addEventListener("click", resultTotal);
+  }
+ //añadir fondo a serie clickada
+function fill (evt) {
+  let clickeado = evt.currentTarget;
+  
+}
+ 
 }
 button.addEventListener("click", searchShows);
 
 
 
 
-//botom para reset, dar id a nuevo boton que haga de refrescar--
-// const reloadButton = document.getElementById("#reload");
-// function reload() {
-//   location.reload();
-// }
-// reloadButton.addEventListener("click", reload, false);
+
+
+
+
+
+
+//boton para reset
+const reloadButton = document.getElementById("#reload");
+function reload() {
+  location.reload();
+}
+reloadButton.addEventListener("click", reload, false);
