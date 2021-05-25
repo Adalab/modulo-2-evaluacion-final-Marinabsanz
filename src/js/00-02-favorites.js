bf0707798354c5fbec9b48d1d514da28913c591b
Marina Benítez Sánchez <marinabsanz@gmail.com>
2021-05-25 10:14:37 +0200

    finish with favorites and start with localstorage

--- src/js/00-02-favorites.js
+++ src/js/00-02-favorites.js
@@ -1,13 +1,83 @@
 //FAVORITAS
 
+//NEXT STEP//
 
-//obtener info d la img seleccionada
-//y dsp guardarlo en array d fav
-//click marca- click desmarca el fav
-//.push añadir elem al array  y dsp remove o splice con nº posicion
-// tb pueds coger el array completo y .filter//tengo q poner la clase fav
-//  en el metodo que pinta todas las tarjetas
-//para hacer eso necesito almacenar los fav
-//donde?en un array aparte
-//UN ARRAY sera original d la api  cucumber
-//otro con cosas d fav
+function listenNowseries() {
+  const allShows = document.querySelectorAll(".js-list");
+  for (const serie of allShows) {
+    serie.addEventListener("click", favoritesChanges);
+
+    // serie.addEventListener("onDragStart", favoritesChanges);    arrastrar---añadir funcion
+  }
+}
+
+function favoritesChanges(evt) {
+  evt.currentTarget; //lo que selecciono
+  const currentSelect = evt.currentTarget; //lo guardo en constante
+  currentSelect.classList.toggle("changeColor"); //añado propiedad nueva de css para cambiar color
+  console.log(currentSelect);
+
+  currentSelect2 = parseInt(currentSelect.id);
+  console.log(currentSelect2);
+  const serieFound = listFav.find(
+    (serie) => serie.show.id === currentSelect2
+  );
+  console.log(serieFound);
+ ///necesito volver a recorrer la
+ // vag global y sacarle algo apra mostrar cuando no este definido
+  
+  if (serieFound ===  undefined) {
+    const serieNoTFound = totalSeries.find(
+      (serie) => serie.show.id === currentSelect2
+    );
+    listFav.push(serieNoTFound);
+  }
+  paintFavoritesTotal()
+  }
+
+///PINTAR FAVORITOS
+
+function paintFavoritesTotal() {
+  let htmlDeUnaSerie = "";
+  totalUlFav.innerHTML = "";
+  for (const serie of listFav) {
+    htmlDeUnaSerie += `<li class="js-list" id="${serie.show.id}">`;
+    htmlDeUnaSerie += serie.show.name + ":" + "</br>";
+    const imagesRslt = serie.show.image;
+    if (imagesRslt === null) {
+      htmlDeUnaSerie = `<img class= "image-js-replace" src= https://www.panatier.es/web/image/product.template/7363/image?unique=d772d4f"></img>"`;
+    } else {
+      htmlDeUnaSerie += `<img class= "image-js-replace" src="${serie.show.image.medium}"/>`;
+    }
+    htmlDeUnaSerie += "</li>";
+//le podria haber dadoa  las 2 el mismo nombre de usershow o serie al empezar y acabar las constantes en la funcion for//
+
+
+    // takeList(userShow); //llamo a la funcion que me llena el array
+  }
+  totalUlFav.innerHTML = htmlDeUnaSerie;
+}
+
+
+
+/////LOCAL STORAGE
+// ---set item de mi listfav y luego el get 
+
+function paintLStorage (){
+  const localStorageFav =localStorage.getItem (favorites);
+  favorites = JSON.parse (localStorageFav);
+  if (favorites === null) {
+    favorites = [];
+  } gotofavResult ();
+}
+
+
+
+///INTENTANDO LO DE ARRASTRAR- DROP DRAG
+//  <div id= "drop-zone" ondrop= "dropHandler(evt) ;">
+//  <p> arrastra y suelta tus pelis aquí</p>
+//  console.log ("drop-zone")
+//  </div>
+
+
+
--- src/js/main.js
+++ src/js/main.js
@@ -17,9 +17,9 @@ function searchShows() {
         //'<h1 class= "js-h1" ${`Estos son tus resultados`} :" + </h1>';  no me funciona -que falla?
       }
 
-      for (const userShow of data) {
+      for (const userShow of totalSeries) {
         let htmlDeUnaSerie = "";
-        htmlDeUnaSerie += '<li class="js-list" id=#id ="${userShow.show.id}">';
+        htmlDeUnaSerie += `<li class="js-list" id="${userShow.show.id}">`;
         htmlDeUnaSerie += userShow.show.name + ":" + "</br>";
         const imagesRslt = userShow.show.image;
         if (imagesRslt === null) {
@@ -38,49 +38,18 @@ function searchShows() {
 
 button.addEventListener("click", searchShows);
 
-//NEXT STEP//
-
-function listenNowseries() {
-  const allShows = document.querySelectorAll(".js-list");
-  for (const serie of allShows) {
-    serie.addEventListener("click", favoritesChanges);
-    serie.addEventListener("draggable", favoritesChanges); //arrastrar---añadir funcion
-  }
-}
-
-function favoritesChanges(evt) {
-  evt.currentTarget; //lo que selecciono
-  const currentSelect = evt.currentTarget; //lo guardo en constante
-  currentSelect.classList.toggle("changeColor"); //añado propiedad nueva de css para cambiar color
-  //no coge las que tienen imagen replace
-  ///seguir
-  const currenteselect2= parseInt(currentSelect.dataset['id']);
-
-  console.log("currenteselect2");
-  // currentSelect.addEventListener ("dragstart", favoritesChanges);
-
-  //no lo cog no sta definido, pero si    
-  // currentSelect.push (id);
-}
-
-// ---------seguir  pasos de handlerclickcard ivan para continuar---
 
 
-// /////LOCAL STORAGE 
-// function paintLStorage (){
-//   const localStorageFav =localStorage.getItem (favorites);
-//   favorites = JSON.parse (localStorageFav);
-//   if (favorites === null) {
-//     favorites = [];
-//   } gotofavResult ();
-// }
+
 // necesito q la funct gotofavResult me pesque  los favoritos selecccionados para dsp guardarlos
 // function gotofavResult()
 // currenteselect2.innerHTML = 'X'
 
-
-
-
+// function onDragStart(event) {
+//   event
+//     .dataTransfer
+//     .setData('Arrastra tus favoritos hacia acá', event.target.id);
+// }
 
 //boton para reset
 const reloadButton = document.getElementById("#reload");
@@ -88,10 +57,6 @@ function reload() {
   location.reload();
 }
 reloadButton.addEventListener("click", reload, false);
-
-
-
-
 
 ////no sirve,
 // function takeList(userShow) {
